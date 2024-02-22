aa1a7ef538f8272132bfdadf8ad9c028cb7913c0
samariddin1661 <150109553+samariddin1661@users.noreply.github.com>
2024-02-22 22:26:45 +0500

    1
    1

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -11,6 +11,9 @@ const Work = () => {
         </div>
         <h1>ABOUT OUR STUDIO</h1>
         <p>It is some systematized exhibition of the whale in his broad genera, that I would now fain put before you. Yet is it no easy task. The classification of the constituents of a chaos, nothing less is here essayed.</p>
+        <div>
+             
+        </div>
       </div>
     </div>
   )
