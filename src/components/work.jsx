bd8fb6544012ae3ab90f2d0482ba1e0223ca04c1
samariddin1661 <150109553+samariddin1661@users.noreply.github.com>
2024-02-26 01:38:42 +0500

    1
    1

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -11,9 +11,18 @@ const Work = () => {
       </div>
         <h1>ABOUT OUR STUDIO</h1>
         <p>It is some systematized exhibition of the whale in his broad genera, that I would now fain put before you. Yet is it no easy task. The classification of the constituents of a chaos, nothing less is here essayed.</p>
-        <div>
-             
-        </div>
+        <tabla className={workcss.table}>
+          <ul>
+            <li><img src="https://www.pirenko-themes.com/hook/architecture/wp-content/uploads/sites/19/2016/06/san-fran-640x640.jpg" alt="" /></li>
+            <li><img src="https://www.pirenko-themes.com/hook/architecture/wp-content/uploads/sites/19/2016/05/awarded-1280x640.jpg" alt="" /></li>
+            <li><img src="https://www.pirenko-themes.com/hook/architecture/wp-content/uploads/sites/19/2016/05/renaissance-640x640.jpg" alt="" /></li>
+            <li><img src="https://www.pirenko-themes.com/hook/architecture/wp-content/uploads/sites/19/2016/06/panorama-1280x640.jpg" alt="" /></li>
+            <li><img src="https://www.pirenko-themes.com/hook/architecture/wp-content/uploads/sites/19/2016/06/penthouse-640x640.jpg" alt="" /></li>
+            <li><img src="https://www.pirenko-themes.com/hook/architecture/wp-content/uploads/sites/19/2016/05/tonal-640x640.jpg" alt="" /></li>
+            <li><img src="" alt="" /></li>
+
+          </ul>
+        </tabla>
       </div>
     </div>
   )
